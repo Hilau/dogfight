c2357bf2be25866322072f8ed9b444dd05e8e2e1
gstal <gstal@10.167.50.195>
2016-05-23 16:21:25 +0200

    Phase 3

--- src/jpu2016/dogfight/view/IViewSystem.java
+++ src/jpu2016/dogfight/view/IViewSystem.java
@@ -1,5 +1,11 @@
 package jpu2016.dogfight.view;
 
 public interface IViewSystem {
-
+	public displayMessage(String Message) {
+		
+	}
+	
+	public default void closeAll(){
+		
+	}
 }
--- src/jpu2016/gameframe/GameFrame.java
+++ src/jpu2016/gameframe/GameFrame.java
@@ -1,5 +1,22 @@
 package jpu2016.gameframe;
 
-public class GameFrame {
+import java.awt.event.KeyEvent;
+import java.util.Observable;
 
+public class GameFrame {
+	public GameFrame(String title, IEventPerformer performer, IGraphicsBuilder graphicsBuilder, Observable observable){
+		
+	}
+	
+	public void keyPressed(KeyEvent keyEvent){
+		
+	}
+	
+	public void keyReleased(KeyEvent keyEvent){
+		
+	}
+	
+	public void keyTyped(KeyEvent keyEvent){
+		
+	}
 }
