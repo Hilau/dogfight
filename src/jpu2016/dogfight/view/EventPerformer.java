71faf03d892bd3386040adf38466c37dc8d8fbcd
gstal <gstal@10.167.50.195>
2016-05-24 17:14:22 +0200

    Phase 4

--- src/jpu2016/dogfight/view/EventPerformer.java
+++ src/jpu2016/dogfight/view/EventPerformer.java
@@ -3,6 +3,7 @@ package jpu2016.dogfight.view;
 import java.awt.event.KeyEvent;
 
 import jpu2016.dogfight.controller.IOrderPerformer;
+import jpu2016.dogfight.controller.Order;
 import jpu2016.dogfight.controller.UserOrder;
 
 public class EventPerformer {
@@ -20,10 +21,50 @@ public class EventPerformer {
 	}
 	
 	private UserOrder keyCodeToUserOrder(int keyCode){
-		/*if(keyCode == 50)
-		{
-			userOrder(Order.UP)
-		}*/
+		switch(keyCode) {
+		
+		case 122 : 
+			UserOrder z = new UserOrder(1, Order.UP);
+		break;
+		
+		case 113 :
+			UserOrder q = new UserOrder(1, Order.LEFT);
+		break;
+		
+		case 100 :
+			UserOrder d = new UserOrder(1, Order.RIGHT);
+		break;
+		
+		case 115 :
+			UserOrder s = new UserOrder(1, Order.DOWN);
+		break;
+		
+		case 32 : 
+			UserOrder space = new UserOrder(1, Order.SHOOT);
+		break;
+		
+		case 1 : 
+			UserOrder un = new UserOrder(2, Order.LEFT);
+		break;
+		
+		case 2 : 
+			UserOrder deux = new UserOrder(2, Order.DOWN);
+		break;
+		
+		case 3 : 
+			UserOrder trois = new UserOrder(2, Order.RIGHT);
+		break;
+		
+		case 5 : 
+			UserOrder quatre = new UserOrder(2, Order.UP);
+		break;
+		
+		case 109 :
+			UserOrder m = new UserOrder(2, Order.SHOOT);
+		break;
+		
+		}
+		
 		
 		return null;
 	}
